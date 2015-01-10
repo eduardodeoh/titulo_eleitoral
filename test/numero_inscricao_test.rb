3d84b4272d7ae8e1d38771d448f541d4875440fb
Eduardo de O. Hernandes <eduardodeoh@gmail.com>
2015-01-10 18:05:05 -0200

    Corrige erros na validação e inserção de testes
    Verificação realizada também através do site http://www.tse.jus.br/eleitor/servicos/situacao-eleitoral/consulta-por-titulo

--- test/numero_inscricao_test.rb
+++ test/numero_inscricao_test.rb
@@ -1,10 +1,8 @@
-# encoding: utf-8
-
 require_relative 'test_helper'
 
 describe TituloEleitoral::NumeroInscricao do
 
-  describe 'dado um número de inscrição válido com 12 dígitos' do
+  describe 'dado o número de inscrição válido - 316820550159' do
     before do
       @valor = 316820550159
       @numero = TituloEleitoral::NumeroInscricao.new(@valor)
@@ -14,12 +12,20 @@ describe TituloEleitoral::NumeroInscricao do
       @numero.valido?.must_equal true
     end
 
-    it 'deve retornar DV1' do
+    it 'deve retornar DV1 == 5' do
       @numero.dv1.must_equal 5
     end
 
-    it 'deve retornar DV2' do
+    it 'deve retornar DV2 == 9' do
       @numero.dv2.must_equal 9
+    end
+
+    it 'deve retornar código UF == 01' do
+      @numero.codigo_uf.must_equal '01'
+    end
+
+    it 'deve retornar sigla UF == SP' do
+      @numero.sigla_uf.must_equal 'SP'
     end
 
     it 'deve retornar número original como array de strings' do
@@ -29,10 +35,6 @@ describe TituloEleitoral::NumeroInscricao do
     it 'deve retornar número como array de strings sem preenchimento de zeros à esquerda' do
       teste = @valor.to_s.split('')
       @numero.numero.must_equal teste
-    end
-
-    it 'deve retornar a UF do título' do
-      @numero.uf.wont_be_nil
     end
   end
 
@@ -47,7 +49,7 @@ describe TituloEleitoral::NumeroInscricao do
     end
   end
 
-  describe 'dado um número com menos de 12 dígitos' do
+  describe 'dado o número - 67735700132 - com menos de 12 dígitos' do
     before do
       @valor3 = 67735700132
       @numero3 = TituloEleitoral::NumeroInscricao.new(@valor3)
@@ -59,7 +61,7 @@ describe TituloEleitoral::NumeroInscricao do
     end
   end
 
-  describe 'dada uma string com mais de 12 dígitos' do
+  describe 'dada a string "0677357001321" com mais de 12 dígitos' do
     before do
       @valor4 = '0677357001321'
       @numero4 = TituloEleitoral::NumeroInscricao.new(@valor4)
@@ -70,7 +72,7 @@ describe TituloEleitoral::NumeroInscricao do
     end
   end
 
-  describe 'dada uma string com dígitos e letras' do
+  describe 'dada a string "6773570013AB" com dígitos e letras' do
     before do
       @valor5 = '6773570013AB'
       @somente_numeros = '6773570013'
@@ -85,7 +87,7 @@ describe TituloEleitoral::NumeroInscricao do
       @numero5.numero_original.must_equal @somente_numeros.to_s.split('')
     end
 
-     it 'número deve ser um array preenchido com zeros' do
+     it 'número deve ser um array de 12 elementos preenchido com zeros à esquerda quando necessário' do
        @numero5.numero.must_equal @somente_numeros.rjust(12, '0').split('')
      end
   end
@@ -93,16 +95,16 @@ describe TituloEleitoral::NumeroInscricao do
   describe 'dada uma string válida de 12 dígitos e dígitos da UF menores que 01 ou maiores que 28' do
     it 'dígitos da UF menor que 01 retorna nil' do
       numero = TituloEleitoral::NumeroInscricao.new('067735700032')
-      numero.uf.must_be_nil
+      numero.codigo_uf.must_be_nil
     end
 
     it 'dígitos da UF maior que 28 retorna nil' do
       numero = TituloEleitoral::NumeroInscricao.new('067735702932')
-      numero.uf.must_be_nil
+      numero.codigo_uf.must_be_nil
     end
   end
 
-  describe 'dada uma string formatada com número de inscrição válido' do
+  describe 'dada a string formatada "3168.2055.0159"' do
     before do
       @valor6 = '3168.2055.0159'
       @numero6 = TituloEleitoral::NumeroInscricao.new(@valor6)
@@ -113,4 +115,17 @@ describe TituloEleitoral::NumeroInscricao do
     end
   end
 
+  describe 'dado um número de inscrição com UF SP ou MG e resto 0 durante cálculo do DV1 ou DV2' do
+    it 'deve retornar como resultado o valor 1 e não 0' do
+      numero = TituloEleitoral::NumeroInscricao.new(113382030116)
+      numero.dv1.must_equal 1
+    end
+  end
+
+  describe 'dado um número de inscrição com UF diferente de SP ou MG e resto 0 durante cálculo do DV1 ou DV2' do
+    it 'deve retornar como resultado o valor igual ao resto da divisão e não 1' do
+      numero = TituloEleitoral::NumeroInscricao.new(113382030308)
+      numero.dv1.must_equal 0
+    end
+  end
 end
